8ee0b76ec2ee8bb728cec46afabf9f1345812523
Jacopo Libè <jacopo.libe@gmail.com>
2021-11-25 21:56:13 +0100

    Working chat

---
+++ NomaiVROnlinePatches/ChatPatches.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using ModTemplate;
+using Sfs2X;
+using UnityEngine.UI;
+using Valve.VR;
+
+namespace NomaiVROnlinePatches
+{
+    public class ChatPatches
+    {
+        private const string k_chatHandlerQualifiedTypeName = "ModTemplate.ChatHandler, OuterWildsOnline";
+        private static SmartFox sfs { get => ConnectionController.Connection; }
+        private static Action UpdateOpenChat = null;
+        private static Type chatHandlerType = AccessTools.TypeByName(k_chatHandlerQualifiedTypeName);
+        private static FieldInfo chatHandlerInputText = AccessTools.Field(chatHandlerType, "inputFieldText");
+        private static InputField hiddenInputField;
+        private static Object chatHandlerInstance;
+
+        [HarmonyPatch(k_chatHandlerQualifiedTypeName, "Start")]
+        [HarmonyPostfix]
+        public static void ChatHandler_Start(Object __instance)
+        {
+            chatHandlerInstance = __instance;
+            SteamVR_Events.System(EVREventType.VREvent_KeyboardClosed).RemoveListener(OnSteamVRKeyboaredClosed);
+            UpdateOpenChat = AccessTools.MethodDelegate<Action>(AccessTools.DeclaredMethod(chatHandlerType, "UpdateOpenChat"), __instance);
+            hiddenInputField = (new UnityEngine.GameObject("VRChatField")).AddComponent<InputField>();
+            SteamVR_Events.System(EVREventType.VREvent_KeyboardClosed).Listen(OnSteamVRKeyboaredClosed);
+        }
+
+        public static void OnSteamVRKeyboaredClosed(VREvent_t evt)
+        {
+            if(hiddenInputField != null && hiddenInputField.IsActive())
+            {
+                var text = new StringBuilder(256);
+                SteamVR.instance.overlay.GetKeyboardText(text, 256);
+                var message = text.ToString();
+                
+                if (!String.IsNullOrEmpty(message.Trim(' ')))
+                {
+                    sfs.Send(new Sfs2X.Requests.PublicMessageRequest(AccessTools.Field(chatHandlerType, "chatMode").GetValue(chatHandlerInstance).ToString() + "ʣ" + message));
+                }
+                hiddenInputField.DeactivateInputField();
+                hiddenInputField.text = "";
+            }
+        }
+
+        [HarmonyPatch(k_chatHandlerQualifiedTypeName, "Update")]
+        [HarmonyPrefix]
+        public static bool ChatHandler_Update(Object __instance)
+        {
+            if(OWInput.IsPressed(InputLibrary.toolActionSecondary))
+            {
+                if(OWInput.IsNewlyPressed(InputLibrary.rollMode))
+                {
+                    UpdateOpenChat();
+                }
+
+                if(OWInput.IsNewlyPressed(InputLibrary.interact))
+                {
+                    hiddenInputField.ActivateInputField();
+                }
+            }
+            return false;
+        }
+    }
+}
--- NomaiVROnlinePatches/NomaiVROnlinePatches.cs
+++ NomaiVROnlinePatches/NomaiVROnlinePatches.cs
@@ -1,29 +1,13 @@
 ﻿using OWML.ModHelper;
-using OWML.Common;
+using HarmonyLib;
 
 namespace NomaiVROnlinePatches
 {
     public class NomaiVROnlinePatches : ModBehaviour
     {
-        private void Awake()
-        {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
-
         private void Start()
         {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(NomaiVROnlinePatches)} is loaded!", MessageType.Success);
-            
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                if (loadScene != OWScene.SolarSystem) return;
-                var playerBody = FindObjectOfType<PlayerBody>();
-                ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!", MessageType.Success);
-            };
+            Harmony.CreateAndPatchAll(typeof(ChatPatches));
         }
     }
 }
