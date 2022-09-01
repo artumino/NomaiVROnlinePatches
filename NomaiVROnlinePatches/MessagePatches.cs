608eca9d404c6781aa6851b2ed0ba60777fcb449
JustOnesAndZeros <benwray2005@gmail.com>
2022-09-01 16:36:42 +0100

    disabled ability to equip the recording tool and removed the button prompt

---
+++ NomaiVROnlinePatches/MessagePatches.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HarmonyLib;
+
+namespace NomaiVROnlinePatches
+{
+    public class MessagePatches
+    {
+        private const string k_MessageHandlerQualifiedTypeName = "OuterWildsOnline.MessageHandler, OuterWildsMMO";
+
+        //disables ability to equip recording tool
+        [HarmonyPatch(k_MessageHandlerQualifiedTypeName, "StartPlacing")]
+        [HarmonyPrefix]
+        public static bool RecordingTool_StartPlacing(Object __instance)
+        {
+            return false;
+        }
+        
+        //disables button prompt
+        [HarmonyPatch(k_MessageHandlerQualifiedTypeName, "OnSuitUp")]
+        [HarmonyPrefix]
+        public static bool RecordingTool_OnSuitUp(Object __instance)
+        {
+            return false;
+        }
+    }
+}
